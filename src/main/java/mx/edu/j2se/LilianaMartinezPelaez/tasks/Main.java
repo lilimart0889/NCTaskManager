6e8ae96ba196911994e72550e82daba1854e0644
Lili <lilimart0808@gmail.com>
2021-02-22 16:57:48 -0600

    Adding files

--- src/main/java/mx/edu/j2se/LilianaMartinezPelaez/tasks/Main.java
+++ src/main/java/mx/edu/j2se/LilianaMartinezPelaez/tasks/Main.java
@@ -1,7 +1,12 @@
 package mx.edu.j2se.LilianaMartinezPelaez.tasks;
 
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.Date;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 	    Persona estudianteNC = new Persona();
 		//System.out.println("Edad:" + estudianteNC.getEdad());
 		estudianteNC.setEdad(14);
@@ -16,7 +21,28 @@ public class Main {
 		System.out.println("Lili:" + loDejaron);
 		System.out.println("Edad:" + estudianteNC2.getEdad());
 
+		String actividad = "", time = "";
 
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Introduzca nueva actividad:\nActividad: ");
+        actividad = entrada.nextLine();
+
+        System.out.println("Fecha en dd/mm/aaaa");
+        time = entrada.nextLine();
+
+		Task Correr = new Task("Correr", 5);
+		System.out.println("\nActividad: " + Correr.getTitle() + " Tiempo: " + Correr.getTime());
+
+		Task Gym = new Task("Gym",1,5,2);
+		System.out.println("\nNueva Actividad: " + Gym.getTitle() + " Start: " + Gym.getStartTime()
+				            + " End: " + Gym.getEndTime() + " Interval: " + Gym.getRepeatInterval());
+
+
+		Date Fecha = new Date();
+		System.out.println("\nLa fecha es: " + Fecha);
+		String strDateFormat = "hh:mm:ss a dd-MMM-aaaa";
+		SimpleDateFormat objSDF = new SimpleDateFormat(strDateFormat);
+		System.out.println("\nLa fecha en otro formato es: " + objSDF.format(Fecha));
 	}
 
 
