96038d27f21fddeb44e1223abed625ca1793040c
Lili <lilimart0808@gmail.com>
2021-03-14 07:59:59 -0600

    Adding files

--- src/main/java/mx/edu/j2se/LilianaMartinezPelaez/tasks/Main.java
+++ src/main/java/mx/edu/j2se/LilianaMartinezPelaez/tasks/Main.java
@@ -1,56 +1,79 @@
 package mx.edu.j2se.LilianaMartinezPelaez.tasks;
 
-import java.text.SimpleDateFormat;
+/*
+@author LiliMP
+Netcracker Training Program
+Practical work no.1: Project Structure
+*/
+
+import mx.edu.j2se.LilianaMartinezPelaez.ejemplos.Tiempo;
+
 import java.util.Scanner;
-import java.util.Date;
 
 public class Main {
-	public static void main(String[] args)
-	{
 
-		//Probando la clase Persona
-		//**************************
+	public static void main(String[] args) {
 
-	    Persona estudianteNC = new Persona();
-		//System.out.println("Edad:" + estudianteNC.getEdad());
-		estudianteNC.setEdad(14);
-		String loDejaron = estudianteNC.loDejanEntrarAlBar();
-		System.out.println("Eduardo:" + loDejaron);
-		System.out.println("Edad:" + estudianteNC.getEdad());
+		// Creating a task object active and non-repetitive
+		Task Running = new Task("Running", 5);
+		System.out.println("Activity: " + Running.getTitle());
+		System.out.println(Running.getTime());
+		System.out.println(Running.isRepeated());
+		System.out.println(Running.isActive());
+		//Returning the next start time of Running execution after current time:
+		System.out.print("The next start time of Running execution is: ");
+		System.out.println(Running.nexTimeAfter(5));
 
-		Persona estudianteNC2 = new Persona();
-		//System.out.println("Edad:" + estudianteNC2.getEdad());
-		estudianteNC2.setEdad(74);
-		loDejaron = estudianteNC2.loDejanEntrarAlBar();
-		System.out.println("Lili:" + loDejaron);
-		System.out.println("Edad:" + estudianteNC2.getEdad());
+		// Creating a task object active and repetitive:
+		Task Gym = new Task("Gym",5,200,10);
+		System.out.println("\nActivity: " + Gym.getTitle() + " Start: " + Gym.getStartTime()
+				          + " End: " + Gym.getEndTime() + " Interval: " + Gym.getRepeatInterval());
+		System.out.println(Gym.isActive());
+		System.out.println(Gym.isRepeated());
+		//Returning the next start time of Gym execution after current time:
+		System.out.print("The next start time of Gym execution is: ");
+		System.out.println(Gym.nexTimeAfter(75));
 
-		//Probando introducir desde teclado actividad y tiempo
-		//***************************************************
+		// Creating a task object inactive and non-repetitive
+		Task Water_plants = new Task("Water plants", 0);
+		System.out.println("\nActivity: " + Water_plants.getTitle());
+		System.out.println(Water_plants.getTime());
+		System.out.println(Water_plants.isActive());
+		System.out.println(Water_plants.isRepeated());
 
-		String actividad = "", time = "";
+		// Creating a task object inactive and repetitive
+		Task Hiking = new Task("Hiking", 0, 12, 2);
+		System.out.println("\nActividad: " + Hiking.getTitle() + " Start: " + Hiking.getStartTime()
+				+ " End: " + Hiking.getEndTime() + " Interval: " + Hiking.getRepeatInterval());
+		System.out.println(Hiking.isActive());
+		System.out.println(Hiking.isRepeated());
 
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("Introduzca nueva actividad:\nActividad: ");
-        actividad = entrada.nextLine();
+		// Creating a task object active and non-repetitive
+		Task Grocery_shop = new Task("Grocery shop",12);
+		System.out.println("\nActivity: " + Grocery_shop.getTitle());
+		System.out.println(Grocery_shop.getTime());
+		System.out.println(Grocery_shop.isActive());
+		System.out.println(Grocery_shop.isRepeated());
+		//Changing Grocery shop task from non-repetitive to repetitive
+		Grocery_shop.setTime(1,9,3);
+		System.out.println("\nUpdated activity: " + Grocery_shop.getTitle() + " Start: " + Grocery_shop.getStartTime()
+				          + " End: " + Grocery_shop.getEndTime() + " Interval: " + Grocery_shop.getRepeatInterval());
+		System.out.println(Grocery_shop.isActive());
+		System.out.println(Grocery_shop.isRepeated());
 
-        System.out.println("Fecha en dd/mm/aaaa");
-        time = entrada.nextLine();
+		// Creating a task object active and repetitive
+		Task Cooking = new Task("Cooking", 2, 9, 3);
+		System.out.println("\nActivity: " + Cooking.getTitle() + " Start: " + Cooking.getStartTime()
+				+ " End: " + Cooking.getEndTime() + " Interval: " + Cooking.getRepeatInterval());
+		System.out.println(Cooking.isActive());
+		System.out.println(Cooking.isRepeated());
+		// Changing Cooking task from repetitive to non-repetitive
+		Cooking.setTime(12);
+		System.out.println("\nUpdated activity: " + Cooking.getTitle());
+		System.out.println(Cooking.getTime());
+		System.out.println(Cooking.isActive());
+		System.out.println(Cooking.isRepeated());
 
-        //Probando el clase Task
-		//***********************
-
-		Task Correr = new Task("Correr", 5);
-		System.out.println("\nActividad: " + Correr.getTitle() + " Tiempo: " + Correr.getTime());
-
-		Task Gym = new Task("Gym",1,5,2);
-		System.out.println("\nNueva Actividad: " + Gym.getTitle() + " Start: " + Gym.getStartTime()
-				            + " End: " + Gym.getEndTime() + " Interval: " + Gym.getRepeatInterval());
-
-		Tiempo temp = new Tiempo();
-		System.out.println("La hora se imprimirá de la forma: " + temp.horaMilitar());
-		temp.ponerTiempo(59,6);
-		System.out.println(temp.horaMilitar());
 	}
 
 }
--- src/main/java/mx/edu/j2se/LilianaMartinezPelaez/tasks/Task.java
+++ src/main/java/mx/edu/j2se/LilianaMartinezPelaez/tasks/Task.java
@@ -1,124 +1,142 @@
 package mx.edu.j2se.LilianaMartinezPelaez.tasks;
 
-public class Task
-{
-        String title;
-        int time;
-        int start;
-        int end;
-        int interval;
-        boolean active;
-        boolean repeat;
-        //int repeticion;
+/*
+@author LiliMP
+Netcracker Training Program
+Practical work no.1: Project Structure
+*/
 
-        //Constructor para tarea NO-RECURSIVA:
-    public Task(String title, int time)
-        {
-            this.title = title;
+public class Task {
+
+    //Instance variables
+    String title;
+    int time;
+    int start;
+    int end;
+    int interval;
+    int current;
+    boolean active;
+    boolean repeat;
+
+    //Constructor for an inactive task to run at a specified time
+    // without repeating with a given name.
+    public Task(String title, int time) {
+        this.title = title;
+        this.time = time;
+    }
+
+    //Constructor for an inactive task to run within the specified time range
+    // (including the start and the end time) with the set repetition interval and
+    // with a given name.
+    public Task(String title, int start, int end, int interval) {
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+    }
+
+    //*********************************
+    // METHODS                        *
+    // ********************************
+
+    //Reading and setting the task name.
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //Reading and setting the task status.
+    public boolean isActive() {
+        if (time ==0 && start == 0) {
+            System.out.print("Active task: ");
+            return active = false;
+        } else {
+            System.out.print("Active task: ");
+            return active = true;
+        }
+    }
+
+    public void setActive(boolean active) {
+            this.active = active;
+    }
+
+    //Methods for reading and changing execution time for non-repetitive tasks.
+    public int getTime() {
+        if (interval != 0) {  // Repetitive task
+            System.out.print("Time: ");
+            return start;
+        } else { // Non-repetitive task
+            System.out.print("Time: ");
+            return time;
+        }
+    }
+
+    public void setTime (int time) { // Change task performance to non-repetitive
+        if (interval != 0 ) {
+            this.end = 0;
+            this.interval = 0;
+            this.start = 0;
             this.time = time;
         }
+    }
 
-        //Constructor para tarea RECURSIVA:
-    public Task(String title, int start, int end, int interval)
-        {
-            this.title = title;
-            this.start = start;
+    //Methods for reading and changing execution time for repetitive tasks.
+    public int getStartTime() {
+        if (interval == 0) { // Non-repetitive task
+            return time;
+        } else { // Repetitive task
+            return start;
+        }
+    }
+
+    public int getEndTime() {
+        if (interval == 0) { // Non-repetitive task
+            return time;
+        } else { // Repetitive task
+            return end;
+        }
+    }
+
+    public int getRepeatInterval() {
+        if (interval == 0) { // Non-repetitive task
+            return 0;
+        } else { // Repetitive task
+            return interval;
+        }
+    }
+
+    public void setTime( int start, int end, int interval) { // Change task performance to repetitive
+        if (time != 0 || time == 0) {
             this.end = end;
             this.interval = interval;
+            this.start = start;
         }
+    }
 
-        //*********************************
-        // CREACIÓN DE MÉTODOS            *
-        // ********************************
-
-        //Método para nombre del Task:
-        public String getTitle ()
-        {
-            return title;
+    //Method for checking the task for repeatability.
+    public boolean isRepeated() {
+        if (interval != 0){
+            System.out.print("Repetitive task: ");
+            return repeat = true;
+        } else {
+            System.out.print("Repetitive task: ");
+            return repeat = false;
         }
+    }
 
-        public void setTitle (String title)
-        {
-            this.title = title;
-        }
-
-        //Métodos para estatus del Task:
-        public boolean isActive () //es un getter
-        {
-            return active;
-        }
-
-        public void setActive ( boolean active)
-        {
-            if (active != true) {
-                System.out.println("Tarea inactiva");
-            } else {
-                this.active = active;
+    //Method that returns the next start time of the task execution after the current time.
+    public int nexTimeAfter(int current) {
+        int nexttimeafter = 0;
+        if (repeat == true && current != end) {
+            for (int i = start; i <= current; i = i + interval) {
+                nexttimeafter = interval + i;
             }
+            return nexttimeafter;
         }
-
-
-        //Métodos para tiempo de ejecución de task no-recursivo:
-        public int getTime ()
-        {
-            if (repeat != false) {
-                return start;
-            } else {
-                return time;
-            }
-
-        }
-
-        public void setTime ( int time)
-        {
-            this.time = time;
-        }
-
-        //Métodos para ejecución de task recursivo:
-        public int getStartTime ()
-        {
-            if (repeat != false) {
-                return time;
-            } else {
-                return start;
-            }
-        }
-
-        public int getEndTime ()
-        {
-            if (repeat != false) {
-                return time;
-            } else {
-                return end;
-            }
-        }
-
-        public int getRepeatInterval ()
-        {
-            if (repeat != false) {
-                return 0;
-            } else {
-                return interval;
-            }
-        }
-
-        public void setTime ( int start, int end, int interval)
-        {
-            if (repeat == false) {
-                this.start = start;
-                this.end = end;
-                this.interval = interval;
-            }
-        }
-
-        //Método para evaluar si el task es recursivo o no
-        public boolean isRepeated () //get
-        {
-            return repeat;
-
-        }
-
-
-
+        return -1;
+    }
 }
 
